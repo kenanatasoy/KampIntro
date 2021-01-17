8c62e6e5fab86d4db9111c6dac842c4a06a9b1ed
kenanatasoy <35261110+kenanatasoy@users.noreply.github.com>
2021-01-18 02:24:04 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";   
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array - dizi;
+
+            string[] kurslar = new string[] 
+            { 
+                "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya başlangıç için temel kurs",
+                "Java",
+                "Python",
+                "C#"
+            };
+
+
+            int i;
+            for (i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine("i değeri kontrol: " + i);
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            
+            
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+            
+        }
+
+        public void Ekle2()
+        {
+
+        
+        }
+    }
+}
